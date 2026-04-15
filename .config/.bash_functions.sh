634423164156b696e86207d132f6a1b7448fbc20
Mads Bay Jensen <madsbayjensen@gmail.com>
2026-04-15 09:53:12 +0000

    very cool bash stuff

--- .config/.bash_functions.sh
+++ .config/.bash_functions.sh
@@ -19,7 +19,6 @@ eddrc() {
     [ -n "${P}" ] && vim "${P}"
 }
 
-
 j() {
 	local FZF_OLD_DEFAULT_COMMAND="$FZF_DEFAULT_COMMAND"
 	export FZF_DEFAULT_COMMAND="fd -c always -d 5 -td -tf -j$(nproc) --no-require-git '.*'"
@@ -32,34 +31,41 @@ j() {
 	export FZF_DEFAULT_COMMAND="$FZF_OLD_DEFAULT_COMMAND"
 }
 
-jd() {
-    
-(RELOAD='reload:rg --column --color=always --smart-case {q} || :'
- fzf --disabled --ansi \
-     --bind "start:$RELOAD" --bind "change:$RELOAD" \
-     --bind 'enter:become:vim {1} +{2}' \
-     --delimiter : \
-     --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
-     --preview-window '~4,+{2}+4/3,<80(right)')
+
+#ripgrep->fzf->vim [QUERY]
+frg() {
+  local T="$(mktemp)"
+  trap "rm -f ${T}; trap - EXIT INT" EXIT INT
+  local R="reload:rg -j$(nproc) --color=always --smart-case {q} "
+  [ ! -t 0 ] && R+="-N ${T}" || R+='--column'
+  local C=(--disabled --ansi --multi --tmux 85%
+		--bind 'alt-a:select-all,alt-d:deselect-all'
+ 		--bind "start:${R} || :" 
+		--bind "change:${R} || :"
+		--delimiter : --query "$*")
+  if [ ! -t 0 ]; then # if pipe then
+    cat > "${T}"
+    fzf "${C[@]}" --bind "enter:become:echo {3..}"
+  else
+    fzf "${C[@]}" --with-nth=1,2 --bind \
+	    'enter:become:[ $FZF_SELECT_COUNT -eq 0 ] && vim {1} +{2} || vim +cw -q {+f}'\
+	--preview 'bat --style=full --color=always --highlight-line {2} {1}'\
+	--preview-window "~4,+{2}+4/3,<80(right)"
+  fi
 }
 
-	#ripgrep->fzf->vim [QUERY]
-frg() (
-  RELOAD='reload:rg'" -j$(nproc) "'--column --color=always --smart-case {q} || :'
-  OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
-            vim {1} +{2}     # No selection. Open the current line in Vim.
-          else
-            vim +cw -q {+f}  # Build quickfix list for the selected items.
-          fi'
-  fzf --disabled --ansi --multi \
-      --bind "start:$RELOAD" --bind "change:$RELOAD" \
-      --bind "enter:become:$OPENER" \
-      --bind "ctrl-o:execute:$OPENER" \
-      --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
-      --delimiter : \
-      --with-nth=1,2 \
-      --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
-      --preview-window '~4,+{2}+4/3,<80(right)' \
-      --query "$*"
-)
+fld() {
+  [ -t 0 ] && echo Input must be from stdin && return -1
+  local T="$(mktemp)"
+  trap "rm -f ${T}; trap - EXIT INT" EXIT INT
+  cat > "${T}"
+  local C="cat ${T}"
+  local R="reload:awk \"{q=\\\"{q}\\\";gsub(/'/,\\\"\\\",q);n=split(q,a);for(i=1;i<=n;i++)printf(\\\"%s \\\",\\\$a[i]);printf(\\\"\\\\n\\\")}\" ${T} || :"
+
+  fzf --disabled --multi \
+	--bind "start:reload:${C}"  \
+	--bind "change:${R}" \
+	--bind "enter:select-all+accept" \
+        --preview "${C}"
+}
 
