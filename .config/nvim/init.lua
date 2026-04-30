4c6401906ac2c4f9ec97bce24cffb1cb345716c4
Mads Bay Jensen <madsbayjensen@gmail.com>
2026-04-30 21:56:34 +0000

    Removed stuff i don't use and organized stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,122 +1,5 @@
--- HELPERS --
+-- THE BASICS --
 local vo = vim.opt
-local c = vim.cmd
-local va = vim.api
-local m = va.nvim_set_keymap
-local bm = va.nvim_buf_set_keymap
-local a = va.nvim_create_autocmd
-local hl = va.nvim_set_hl
-local fn = vim.fn
-local fs = vim.fs
-local lse = vim.lsp.enable
-local lsc = vim.lsp.config
-local lsb = vim.lsp.buf
-local o_snr = {silent = true, noremap = true}
-local o_sr = {silent = true}
-
-local function nm(x, y) m('n', x, y, o_snr) end
-local function im(x, y) m('i', x, y, o_snr) end
-local function vm(x, y) m('v', x, y, o_snr) end
-local function nmr(x, y) m('n', x, y, o_sr) end
-local function imr(x, y) m('i', x, y, o_sr) end
-local function vmr(x, y) m('v', x, y, o_sr) end
-
-local function bnm(x, y) m('n', x, y, o_snr) end
-local function bim(x, y) m('i', x, y, o_snr) end
-local function bvm(x, y) m('v', x, y, o_snr) end
-
--- PLUGINS --
-vim.pack.add({
-	{ src = "https://github.com/Saghen/blink.cmp"},
-	{ src = "https://github.com/catppuccin/nvim"},
-	{ src = "https://github.com/christoomey/vim-tmux-navigator"},
-	{ src = "https://github.com/folke/snacks.nvim"},
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-	{ src = "https://github.com/neovim/nvim-lspconfig"},
-	{ src = "https://github.com/luukvbaal/nnn.nvim"},
-	{ src = "https://github.com/rafamadriz/friendly-snippets"}
-})
-
-require 'snacks' .setup {
-	image = {
-		formats = {
-			"png",
-			"jpg",
-			"jpeg",
-			"webp",
-			"pdf",
-		},
-		force = false,
-		doc = {
-			enabled = true,
-			-- only used if `opts.inline` is disabled
-			float = true,
-			max_width = 80,
-			max_height = 40,
-			conceal = function(lang, type)
-				return type == "math"
-			end,
-		},
-		img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments" },
-	}
-}
-
-require 'blink.cmp'.setup {
-	appearance = { nerd_font_variant = 'mono' },
-	completion = { documentation = { auto_show = true } },
-	keymap = { preset = 'default' },
-    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' }, },
-    fuzzy = { implementation = "lua" }
-}
-
-require 'nvim-treesitter.config'.setup {
-		highlight = { 
-			enable = true,
-			additional_vim_regex_highlighting = false, 
-            max_file_length = 10000,
-		},
-		indent = { enable = true, },
-}
-
-vim.treesitter.language.register('markdown_inline', 'md')
-vim.treesitter.language.register('python', 'py')
-vim.treesitter.language.register('json', 'json')
-
-lsc('clangd', {
-  cmd = {
-    'clangd',
-    '--background-index',
-    '--clang-tidy',
-    '--header-insertion=iwyu',
-    '--completion-style=detailed',
-    '--function-arg-placeholders=true',
-  },
-  filetypes = { 'c', 'cpp' },
-  root_markers = {
-    '.clangd',
-    'compile_commands.json',
-    'compile_flags.txt',
-    '.git'
-  },
-  capabilities = { offsetEncoding = { 'utf-16' }, },
-})
-lse('clangd')
-
-lsc('luals', {
-	cmd = { 'lua-language-server' },
-	filetypes = { 'lua' },
-	root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml' },
-	settings = {
-		Lua = {
-			runtime = {
-				version = 'LuaJIT'
-			},
-		}
-	}
-})
-lse('luals')
-
--- OPTIONS --
 vo.autoindent     = true
 vo.autoread       = true
 vo.clipboard      = 'unnamedplus'
@@ -135,117 +18,77 @@ vo.softtabstop    = 4
 vo.tabstop        = 4
 vo.winborder      = "rounded"
 
+-- PLUGINS --
+vim.pack.add({
+	 "https://github.com/catppuccin/nvim",
+	 "https://github.com/christoomey/vim-tmux-navigator",
+	 "https://github.com/nvim-treesitter/nvim-treesitter" ,
+})
+
+-- COLORSCHEME --
+local c = vim.cmd
+local va = vim.api
+local hl = va.nvim_set_hl
 c.colorscheme("catppuccin")
 hl(0, "LineNrAbove", { fg = "#89dceb" })
 hl(0, "LineNrBelow", { fg = "#89dceb" })
 hl(0, "LineNr", { fg = "#fab387" })
---hl(0, "Normal", { bg = "1e1e2e" })
 hl(0, "NormalFloat", { bg = "none" })
 hl(0, "StatusLine", { bg = "none" })
 
--- keys --
+-- HELPERS --
+require 'nvim-treesitter.config'.setup {
+	highlight = { 
+		enable = true,
+		additional_vim_regex_highlighting = false, 
+    		max_file_length = 10000,
+	},
+	indent = { enable = true, },
+}
+
+vim.g.markdown_fenced_languages = {'python', 'cpp', 'c', 'bash', 'awk'}
+
+local vtl = vim.treesitter.language.register
+vtl('markdown_inline', 'md')
+vtl('python', 'py')
+vtl('json', 'json')
+
+
+-- KEYS --
+local m = va.nvim_set_keymap
+local function nm(x, y) m('n', x, y, {silent = true, noremap = true}) end
+local function im(x, y) m('i', x, y, {silent = true, noremap = true}) end
+local function vm(x, y) m('v', x, y, {silent = true, noremap = true}) end
+local function nmr(x, y) m('n', x, y, {silent = true}) end
+local function imr(x, y) m('i', x, y, {silent = true}) end
+local function vmr(x, y) m('v', x, y, {silent = true}) end
+
 vim.g.mapleader = " "
-
-local nnn = require("nnn").builtin
-require("nnn").setup({
-  mappings = {
-    { "<C-t>", nnn.open_in_tab },
-    { "<C-s>", nnn.open_in_split },
-    { "<C-v>", nnn.open_in_vsplit },
-    { "<C-p>", nnn.open_in_preview },
-    { "<C-y>", nnn.copy_to_clipboard },
-    { "<C-w>", nnn.cd_to_path },
-    { "<C-e>", nnn.populate_cmdline },
-  }
-})
-
--- file navigation
-nm('<leader>fc',':e ~/.config/nvim/init.lua<CR>')
-nm('_', ':NnnExplorer<CR>')
-nm('-', ':NnnPicker<CR>')
-
--- make
-nm('<leader>mm',':make<CR>')
-nm('<leader>mc',':make clean<CR>')
-nm('<leader>mr',':make run<CR>')
+nm('<leader>rc', ':lua vim.cmd("edit " .. vim.fn.system("~/tools/edrc"):gsub("%s+$", ""))<CR>') 
 
 -- buffers / panes
 nm('<leader>bk',':bd<CR>')
+nm('<leader>bd',':bd<CR>')
 nm('<leader>bp',':bp<CR>')
 nm('<leader>bn',':bn<CR>')
+nm('<Tab>',':bn<CR>')
+nm('<S-Tab>',':bp<CR>')
+nm('-',':vs<CR>')
+nm('_',':sp<CR>')
+
 nm('<C-h>',':TmuxNavigateLeft<CR>')
 nm('<C-l>',':TmuxNavigateRight<CR>')
 nm('<C-k>',':TmuxNavigateUp<CR>')
 nm('<C-j>',':TmuxNavigateDown<CR>')
-
 nm('<ESC>',':nohlsearch<CR>')
 
-local table = {
-	['('] = ')',
-	['['] = ']',
-	['{'] = '}',
-	['<'] = '>',
-	['"'] = '"',
-	["'"] = "'"
-}
-
-for x, y in pairs(table) do
-	im(x, x .. y .. '<Left>')
-	im(x .. '<CR>', x ..  '<CR><CR>'.. y .. '<Up>    ')
-	im(x .. '<BS>', x)
-end
-
-vm('<leader>a', ':!awk \'\'<Left>')
-vm('<leader>n', ':norm ')
-
-
-nmr('<Tab>', '<Esc>/[)\\}"\'>]<CR><ESC>a')
-nmr('<S-Tab>', '<Esc>?[([{"\'<]<CR><ESC>a')
-
--- AUTO CMDS -- 
-local root_markers = { '.git', '.clangd', 'Makefile'}
-
-a("BufEnter", {
-	callback = function()
-		local r = fs.find(root_markers, { upward = true })[1]
-		if r then
-			fn.chdir(fs.dirname(r))
-		end
-	end
-})
-
-a('LspAttach', {
-	pattern = { '*' },
-	callback = function(event)
-		bnm('gd',        ':lua vim.lsp.buf.definition()<CR>', {buffer = event.buf})
-		bnm('gr',        ':lua vim.lsp.buf.references()<CR>', {buffer = event.buf})
-		nm('gI',         ':lua vim.lsp.buf.implementation()<CR>')
-		nm('<leader>D',  ':lua vim.lsp.buf.type_definition()<CR>')
-		nm('<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
-		nm('<leader>ca', ':lua vim.lsp.buf.code_action()<CR>')
-		nm('C-K',':lua vim.lsp.buf.hover()<CR>')
-	end, })
-
-a('LspAttach', {
-	pattern = { '*.c', '*.cpp', '*.h', '*.hpp' },
-	callback = function()
-		nm('<leader>sh', ':LspClangdSwitchSourceHeader<CR>')
-		nm('<leader>st', ':LspClangdTypeHierarchy<CR>')
-		nm('<leader>sm', ':LspClangdMemoryUsage<CR>')
-		nm('<leader>ss', ':LspClangdSymbolInfo<CR>')
-	end,
-})
-
-a('BufWritePre', {
-  pattern = { '*.c', '*.cpp', '*.h', 'hpp' },
+-- AUTOCOMMANDS --
+local a = va.nvim_create_autocmd
+a({'BufEnter', 'BufWinEnter'}, {
+  pattern = '*.md',
   callback = function()
-    lsb.format()
-  end,
-})
-
-a('FileType', {
-  pattern = 'c',
-  callback = function()
-    vim.wo.conceallevel = 2
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
   end
 })
+
